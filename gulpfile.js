5a35ae5d469fe55a010d14f2d6cc6312277bea18
Evgeniya Malyushkina <e.malyushkina@mail.ru>
2020-09-29 11:18:29 +0800

    Настроила сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const autoprefixer = require("autoprefixer");
 const csso = require("gulp-csso");
 const sync = require("browser-sync").create();
 const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const htmlmin = require("gulp-htmlmin");
 
 const styles = () => {
   return gulp
@@ -48,3 +51,59 @@ const watcher = () => {
 };
 
 exports.default = gulp.series(styles, server, watcher);
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}").pipe(
+    imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3,
+      }),
+      imagemin.mozjpeg({
+        progressive: true,
+      }),
+      imagemin.svgo(),
+    ])
+  );
+};
+exports.images = images;
+const copy = () => {
+  return gulp
+    .src(
+      [
+        "source/fonts/**/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/*.ico",
+        "source/*.html",
+        "source/css/**"
+      ],
+      {
+        base: "source",
+      }
+    )
+    .pipe(gulp.dest("build"));
+};
+exports.copy = copy;
+
+// удалить старую версию build
+
+const clean = () => {
+  return del("build");
+};
+exports.clean = clean;
+
+const html = () => {
+  return gulp
+    .src("source/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest("build"));
+};
+exports.html = html;
+
+const build = gulp.series(clean, styles, images, html, copy);
+
+exports.build = build;
